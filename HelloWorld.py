f398996843815fd9a785ad5b3840907725ea8066
Nicole <nyarbrough14@gmail.com>
2021-08-10 15:18:38 -0700

    Build models
    Testing logistic regression, linear discriminant analysis, k-nearest neighbors, classification and regression trees, gaussian naive bayes, and support vector machines.

--- HelloWorld.py
+++ HelloWorld.py
@@ -48,18 +48,62 @@ dataset = read_csv(url, names=names)
 
 # head
 #print(dataset.head(20))
+'''
 
+'''
 # descriptions
 print(dataset.describe())
+'''
 
+'''
 # class distribution
 print(dataset.groupby('class').size())
+'''
 
+'''
 # box and whisker plots
 dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
 pyplot.show()
+'''
 
+'''
 # histograms
 dataset.hist()
 pyplot.show()
 '''
+'''
+# scatter plot matrix
+scatter_matrix(dataset)
+pyplot.show()
+'''
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:,0:4]
+y = array[:,4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+
+'''
+print("X_train", X_train)
+print("X_validation", X_validation)
+print("Y_train", Y_train)
+print("Y_validation", Y_validation)
+'''
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr'))) # Logistic Regression
+models.append(('LDA', LinearDiscriminantAnalysis())) # Linear Discriminant Analysis
+models.append(('KNN', KNeighborsClassifier())) # K-Nearest Neighbors
+models.append(('CART', DecisionTreeClassifier())) # Classification and Regression Trees
+models.append(('NB', GaussianNB())) # Gaussian Naive Bayes
+models.append(('SVM', SVC(gamma='auto'))) # Support Vector Machines
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+	kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+	cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+	results.append(cv_results)
+	names.append(name)
+	print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
