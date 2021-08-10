19af4b26d0791ac117d81c3d70c27da7a866a609
Nicole <nyarbrough14@gmail.com>
2021-08-10 15:25:31 -0700

    Complete example and evaluate predictions

--- HelloWorld.py
+++ HelloWorld.py
@@ -107,3 +107,20 @@ for name, model in models:
 	results.append(cv_results)
 	names.append(name)
 	print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
+'''
+# Compare Algorithms
+pyplot.boxplot(results, labels=names)
+pyplot.title('Algorithm Comparison')
+pyplot.show()
+'''
+
+# Make predictions on validation dataset
+model = SVC(gamma='auto')
+model.fit(X_train, Y_train)
+predictions = model.predict(X_validation)
+
+# Evaluate predictions
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
